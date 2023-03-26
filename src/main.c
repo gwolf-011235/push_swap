1ad4e82ec79e2419d7fbc82d51a2161973cca7e9
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-26 10:23:40 +0200

    feat: added instance of struct stack, call of parse ft

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 07:04:28 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/26 09:40:57 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/26 10:15:44 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,18 @@
 
 int	main(int argc, char *argv[])
 {
-	int	count;
+	t_stack stack;
 
 	if (argc == 1)
 	{
 		ft_printf("Usage: %s <list of integers>\n", argv[0]);
 		exit(0);
 	}
-	count = argc - 1;
-	if (ft_check_input(argc, argv, &count))
+	stack.nums = argc - 1;
+	if (ft_check_input(argc, argv, &stack.nums))
 	{
 		ft_printf("All nums are valid!\n");
-		ft_printf("This is count: %d\n", count);
+		ft_printf("This is count: %d\n", stack.nums);
 	}
+	ft_parse_input(&stack, argc, argv);
 }

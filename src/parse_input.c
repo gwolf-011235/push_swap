fc7473458bdc044862468f5d681f8e4a76f3f8c8
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-26 16:44:36 +0200

    feat: new ft_check_doubles

--- src/parse_input.c
+++ src/parse_input.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 09:50:42 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/26 10:50:31 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/26 15:32:39 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,17 @@ void	ft_parse_input(t_stack *stack, int argc, char **argv)
 	ft_malloc_stack(stack);
 	ft_extract_nums(stack, argc, argv);
 	ft_print_stacks(stack);
-
-
+	ft_check_doubles(stack);
 }
 
 void	ft_malloc_stack(t_stack *stack)
 {
 	stack->a = malloc(sizeof(int) * stack->nums + 1);
 	if (!stack->a)
-	{
-		write(1, "Error\n", 6);
-		exit(1);
-	}
+		ft_terminate();
 	stack->b = malloc(sizeof(int) * stack->nums + 1);
 	if (!stack->b)
-	{
-		free(stack->a);
-		write(1, "Error\n", 6);
-		exit(1);
-	}
+		ft_free_and_terminate(stack);
 	ft_memset(stack->a, 0, (stack->nums + 1) * 4);
 	ft_memset(stack->b, 0, (stack->nums + 1) * 4);
 }
@@ -60,4 +52,23 @@ void	ft_extract_nums(t_stack *stack, int argc, char **argv)
 	}
 	stack->a_count = stack->nums;
 	stack->b_count = 0;
+}
+
+void	ft_check_doubles(t_stack *stack)
+{
+	uint32_t	i;
+	uint32_t	j;
+
+	i = 0;
+	while (i < stack->nums)
+	{
+		j = i + 1;
+		while (j < stack->nums)
+		{
+			if (stack->a[i] == stack->a[j])
+				ft_free_and_terminate(stack);
+			j++;
+		}
+		i++;
+	}
 }

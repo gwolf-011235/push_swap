bff2d75c81aff7ec2ca44d4b71a1391679ea7d7e
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-26 16:45:38 +0200

    fix: added new files and prototypes

--- Makefile
+++ Makefile
@@ -42,7 +42,8 @@ ECHO = printf "\033[2K\r[`expr ${HIT_COUNT} '*' 100 / ${HIT_TOTAL}`%%] %s"
 SRC :=  main.c \
 		check_input.c \
 		parse_input.c \
-		utils_print.c
+		utils_print.c \
+		terminate.c
 SRCS := $(addprefix $(SRC_DIR)/, $(SRC))
 
 # objects
--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 07:04:49 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/26 10:49:56 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/26 15:32:20 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,12 @@ int		ft_move_like_atoi(char *str);
 void	ft_parse_input(t_stack *stack, int argc, char **argv);
 void	ft_malloc_stack(t_stack *stack);
 void	ft_extract_nums(t_stack *stack, int argc, char **argv);
+void	ft_check_doubles(t_stack *stack);
 
 void	ft_print_stacks(t_stack *stack);
 
+
+void	ft_terminate(void);
+void	ft_free_and_terminate(t_stack *stack);
 
 #endif

2155542a3c792ec41fd039197a120028cc039e9c
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-26 10:51:54 +0200

    feat: added more info to print util

--- src/utils_print.c
+++ src/utils_print.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 10:05:10 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/26 10:21:01 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/26 10:46:19 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	ft_print_stacks(t_stack *stack)
 {
 	uint32_t	i;
 
+	ft_printf("The stacks are\n");
 	i = 0;
 	while (i < stack->nums)
 	{
@@ -28,5 +29,8 @@ void	ft_print_stacks(t_stack *stack)
 		if (i >= stack->a_count && i >= stack->b_count)
 			break ;
 	}
-	ft_printf("_____ _____\n  a     b\n");
+	ft_printf("_____ _____\n  a     b\n\n");
+	ft_printf("Sum of integers: %d\n", stack->nums);
+	ft_printf("Integers in stack a: %d\n", stack->a_count);
+	ft_printf("Integers in stack b: %d\n", stack->b_count);
 }

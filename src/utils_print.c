e860a1e4d0e598e31f2909ad484b96fd4100b703
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-26 20:22:16 +0200

    fix: print should now work with 0

--- src/utils_print.c
+++ src/utils_print.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 10:05:10 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/26 16:58:48 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/26 20:20:59 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,25 @@
 void	ft_print_stacks(t_stacks *stacks)
 {
 	uint32_t	i;
+	int64_t		temp_a;
+	int64_t		temp_b;
 
-	ft_printf("The stacks are\n");
 	i = 0;
+	temp_a = stacks->a.size;
+	temp_b = stacks->b.size;
+	ft_printf("The stacks are\n\n");
 	while (i < stacks->nums)
 	{
-		if (stacks->a.array[i] != 0)
+		if (temp_a > 0 && temp_b > 0)
+			ft_printf("\t%5d\t%5d", stacks->a.array[i], stacks->b.array[i]);
+		else if (temp_a > 0)
 			ft_printf("\t%5d", stacks->a.array[i]);
-		if (stacks->b.array[i] != 0)
-			ft_printf("\t%5d", stacks->b.array[i]);
+		else if (temp_b > 0)
+			ft_printf("\t\t%5d", stacks->b.array[i]);
+		temp_a--;
+		temp_b--;
 		ft_printf("\n");
 		i++;
-		if (i >= stacks->a.size && i >= stacks->b.size)
-			break ;
 	}
 	ft_printf("\t_____\t_____\n\t  a  \t  b\n\n");
 	ft_printf("Sum of integers: %d\n", stacks->nums);

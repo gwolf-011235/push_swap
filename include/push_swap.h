e296bb79f03503e873b0a24d771c3e6d11b00e93
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-26 17:00:30 +0200

    feat: new struct stacks and rework of struct stack

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 07:04:49 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/26 15:32:20 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/26 16:56:37 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,29 @@
 # include "ft_print.h"
 
 typedef struct	s_stack {
-	uint32_t	nums;
-	int			*a;
-	uint32_t	a_count;
-	int			*b;
-	uint32_t	b_count;
+	uint32_t	size;
+	int			*array;
 }	t_stack;
+
+typedef struct	s_stacks {
+	uint32_t	nums;
+	t_stack		a;
+	t_stack		b;
+}	t_stacks;
 
 bool	ft_check_input(int argc, char **argv, uint32_t *count);
 bool	ft_is_valid_num(char *str);
 int		ft_move_like_atoi(char *str);
 
-void	ft_parse_input(t_stack *stack, int argc, char **argv);
-void	ft_malloc_stack(t_stack *stack);
-void	ft_extract_nums(t_stack *stack, int argc, char **argv);
-void	ft_check_doubles(t_stack *stack);
+void	ft_parse_input(t_stacks *stacks, int argc, char **argv);
+void	ft_malloc_stacks(t_stacks *stacks);
+void	ft_extract_nums(t_stacks *stacks, int argc, char **argv);
+void	ft_check_doubles(t_stacks *stacks);
 
-void	ft_print_stacks(t_stack *stack);
+void	ft_print_stacks(t_stacks *stacks);
 
 
 void	ft_terminate(void);
-void	ft_free_and_terminate(t_stack *stack);
+void	ft_free_and_terminate(t_stacks *stacks);
 
 #endif

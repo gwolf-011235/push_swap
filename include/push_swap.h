93b1b2bd5245046260e97445d95ced7213ce31bc
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-26 09:43:06 +0200

    feat: created header file

---
+++ include/push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/26 07:04:49 by gwolf             #+#    #+#             */
+/*   Updated: 2023/03/26 09:38:17 by gwolf            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "ft_print.h"
+
+bool	ft_check_input(int argc, char **argv, int *count);
+bool	ft_is_valid_num(char *str);
+int		ft_move_like_atoi(char *str);
+
+#endif

6e990619e970ecabba484b45817d01e628bbfe9f
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-27 23:14:51 +0200

    feat: new file to prep input: assign keys for the values and copy these keys into stack a

---
+++ include/prep_input.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prep_input.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/27 23:02:28 by gwolf             #+#    #+#             */
+/*   Updated: 2023/03/27 23:03:56 by gwolf            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PREP_INPUT_H
+# define PREP_INPUT_H
+
+# include "push_swap.h"
+
+void			ft_prep_input(t_stacks *stacks);
+static void		ft_assign_keys(t_stacks *stacks);
+static uint32_t	ft_find_next_max(t_store *store, uint32_t nums);
+
+#endif
---
+++ src/prep_input.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prep_input.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/27 22:40:37 by gwolf             #+#    #+#             */
+/*   Updated: 2023/03/27 23:12:00 by gwolf            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "prep_input.h"
+
+void	ft_prep_input(t_stacks *stacks)
+{
+	ft_assign_keys(stacks);
+	ft_memcpy(stacks->a.array, stacks->store.key, sizeof(int32_t) * stacks->nums);
+	stacks->a.size = stacks->nums;
+	stacks->b.size = 0;
+}
+
+static void	ft_assign_keys(t_stacks *stacks)
+{
+	uint32_t	next_key;
+	uint32_t	next_index;
+	uint32_t	i;
+
+	next_key = stacks->nums;
+	i = 0;
+	while (i < stacks->nums)
+	{
+		next_index = ft_find_next_max(&stacks->store, stacks->nums);
+		stacks->store.key[next_index] = next_key;
+		next_key--;
+		i++;
+	}
+}
+
+static uint32_t	ft_find_next_max(t_store *store, uint32_t nums)
+{
+	int32_t		cur_max;
+	uint32_t	cur_max_i;
+	uint32_t	i;
+
+	cur_max = store->value[0];
+	cur_max_i = 0;
+	i = 0;
+	while (i < nums)
+	{
+		if (store->value[i] > cur_max && store->key[i] == 0)
+		{
+			cur_max = store->value[i];
+			cur_max_i = i;
+		}
+		i++;
+	}
+	return (cur_max_i);
+}

1215b6e508bc7f98a9500b71b08f92c8688beb22
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-29 00:47:11 +0200

    feat: made moves smaller and added control function

--- src/moves.c
+++ src/moves.c
@@ -1,87 +1,77 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ops.c                                              :+:      :+:    :+:   */
+/*   moves.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 19:37:50 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/26 20:39:23 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/29 00:34:13 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_swap(t_stacks *stacks, uint8_t option)
+void	ft_bust_a_move(uint8_t move, t_stacks *stacks)
 {
-	int	temp;
-
-	if ((option == A || option == BOTH) && stacks->a.size > 1)
-	{
-		temp = stacks->a.array[0];
-		stacks->a.array[0] = stacks->a.array[1];
-		stacks->a.array[1] = temp;
-	}
-	if ((option == B || option == BOTH) && stacks->b.size > 1)
-	{
-		temp = stacks->b.array[0];
-		stacks->b.array[0] = stacks->b.array[1];
-		stacks->b.array[1] = temp;
-	}
+	if (move == SA)
+		ft_swap(&stacks->a);
+	else if (move == SB)
+		ft_swap(&stacks->b);
+	else if (move == PA)
+		ft_push(&stacks->a, &stacks->b);
+	else if (move == PB)
+		ft_push(&stacks->b, &stacks->a);
+	else if (move == RA)
+		ft_rotate(&stacks->a);
+	else if (move == RB)
+		ft_rotate(&stacks->b);
+	else if (move == RRA)
+		ft_rev_rotate(&stacks->a);
+	else if (move == RRB)
+		ft_rev_rotate(&stacks->b);
+	if (!ft_enqueue(&stacks->moves, ft_ret_move_string(move)))
+		ft_free_and_terminate(stacks);
 }
 
-void	ft_push(t_stacks *stacks, uint8_t option)
+void	ft_swap(t_stack *stack)
 {
-	if (option == A && stacks->b.size > 0)
-	{
-		ft_memmove(stacks->a.array + 1, stacks->a.array, stacks->a.size * sizeof(int));
-		stacks->a.array[0] = stacks->b.array[0];
-		ft_memmove(stacks->b.array, stacks->b.array + 1, stacks->b.size * sizeof(int));
-		stacks->a.size++;
-		stacks->b.size--;
-	}
-	else if (option == B && stacks->a.size > 0)
-	{
-		ft_memmove(stacks->b.array + 1, stacks->b.array, stacks->b.size * sizeof(int));
-		stacks->b.array[0] = stacks->a.array[0];
-		ft_memmove(stacks->a.array, stacks->a.array + 1, stacks->a.size * sizeof(int));
-		stacks->a.size--;
-		stacks->b.size++;
-	}
+	uint32_t	temp;
+
+	if (stack->size < 2)
+		return ;
+	temp = stack->array[0];
+	stack->array[0] = stack->array[1];
+	stack->array[1] = temp;
 }
 
-void	ft_rotate(t_stacks *stacks, uint8_t option)
+void	ft_push(t_stack *dst, t_stack *src)
 {
-	int	temp;
+	if (src->size < 0)
+		return ;
+	ft_memmove(dst->array + 1, dst->array, dst->size * sizeof(int32_t));
+	dst->array[0] = src->array[0];
+	ft_memmove(src->array, src->array + 1, src->size * sizeof(int32_t));
+	dst->size++;
+	src->size--;
 
-	if ((option == A || option == BOTH) && stacks->a.size > 1)
-	{
-		temp = stacks->a.array[0];
-		ft_memmove(stacks->a.array, stacks->a.array + 1, stacks->a.size * sizeof(int));
-		stacks->a.array[stacks->a.size - 1] = temp;
-	}
-	if ((option == B || option == BOTH) && stacks->b.size > 1)
-	{
-		temp = stacks->b.array[0];
-		ft_memmove(stacks->b.array, stacks->b.array + 1, stacks->b.size * sizeof(int));
-		stacks->b.array[stacks->b.size - 1] = temp;
-	}
 }
 
-void	ft_rev_rotate(t_stacks *stacks, uint8_t option)
+void	ft_rotate(t_stack *stack)
 {
-	int	temp;
+	uint32_t	temp;
 
-	if ((option == A || option == BOTH) && stacks->a.size > 1)
-	{
-		temp = stacks->a.array[stacks->a.size - 1];
-		ft_memmove(stacks->a.array + 1, stacks->a.array, stacks->a.size * sizeof(int));
-		stacks->a.array[0] = temp;
-	}
-	if ((option == B || option == BOTH) && stacks->b.size > 1)
-	{
-		temp = stacks->b.array[stacks->b.size - 1];
-		ft_memmove(stacks->b.array + 1, stacks->b.array, stacks->b.size * sizeof(int));
-		stacks->b.array[0] = temp;
-	}
+	temp = stack->array[0];
+	ft_memmove(stack->array, stack->array + 1, stack->size * sizeof(int32_t));
+	stack->array[stack->size - 1] = temp;
+}
+
+void	ft_rev_rotate(t_stack *stack)
+{
+	uint32_t	temp;
+
+	temp = stack->array[stack->size - 1];
+	ft_memmove(stack->array + 1, stack->array, stack->size * sizeof(int32_t));
+	stack->array[0] = temp;
+
 }

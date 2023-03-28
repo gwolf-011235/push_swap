684b546486d9561c94908e09bc87256d7586d98a
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-03-29 01:06:45 +0200

    feat: new file for simple algorithms

---
+++ src/sort_simple.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_simple.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/27 23:56:24 by gwolf             #+#    #+#             */
+/*   Updated: 2023/03/28 00:09:35 by gwolf            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_sort_two(t_stacks *stacks)
+{
+	if (stacks->a.array[0] > stacks->a.array[1])
+		ft_swap(stacks, A);
+}
+
+void	ft_sort_three(t_stacks *stacks)
+{
+
+}

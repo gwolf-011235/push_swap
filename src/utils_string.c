a1b4518b4612ee1f0ed344c82a3f3e0d59a105ce
Gabriel <gwolf@student.42vienna.com>
2023-03-30 17:55:28 +0200

    feat: new ft find from top

--- src/utils_string.c
+++ src/utils_string.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils_string.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
+/*   By: gwolf < gwolf@student.42vienna.com >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 19:23:55 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/26 19:25:02 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/30 16:14:19 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,4 +24,20 @@ int	ft_move_like_atoi(char *str)
 	while (ft_isdigit(str[i]))
 		i++;
 	return (i);
+}
+
+uint32_t	ft_search_from_top(t_stack *stack, int32_t num)
+{
+	uint32_t	ret;
+	uint32_t	i;
+
+	ret = INT_MAX;
+	i = 0;
+	while (i < stack->size)
+	{
+		if (stack->array[i] == num)
+			ret = i;
+		i++;
+	}
+	return (ret);
 }

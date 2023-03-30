27c2d2096a55d290a352d92c94aea7d41536bc7f
Gabriel <gwolf@student.42vienna.com>
2023-03-30 14:13:25 +0200

    fix: correct header for check_input

--- include/check_input.h
+++ include/check_input.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   check.h                                            :+:      :+:    :+:   */
+/*   check_input.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
+/*   By: gwolf <gwolf@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 19:29:10 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/26 19:33:02 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/03/30 13:29:16 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "push_swap.h"
 
-bool		ft_check_input(int argc, char **argv, uint32_t *count);
+bool		ft_check_input(int argc, char **argv, uint32_t *nums);
 static bool	ft_is_valid_num(char *str);
 
 #endif

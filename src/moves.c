427e2014391598c5ae03570e5ea32b1cb227a120
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-04-03 22:25:22 +0200

    fix: comp with zero; return if empty

--- src/moves.c
+++ src/moves.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   moves.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gwolf < gwolf@student.42vienna.com >       +#+  +:+       +#+        */
+/*   By: gwolf <gwolf@student.42vienna.com >        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/26 19:37:50 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/31 09:13:49 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/04/03 22:09:34 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	ft_swap(t_stack *stack)
 
 void	ft_push(t_stack *dst, t_stack *src)
 {
-	if (src->size < 0)
+	if (src->size == 0)
 		return ;
 	ft_memmove(dst->array + 1, dst->array, dst->size * sizeof(int32_t));
 	dst->array[0] = src->array[0];
@@ -38,6 +38,8 @@ void	ft_rotate(t_stack *stack)
 {
 	uint32_t	temp;
 
+	if (stack->size < 2)
+		return ;
 	temp = stack->array[0];
 	ft_memmove(stack->array, stack->array + 1, stack->size * sizeof(int32_t));
 	stack->array[stack->size - 1] = temp;
@@ -47,6 +49,8 @@ void	ft_rev_rotate(t_stack *stack)
 {
 	uint32_t	temp;
 
+	if (stack->size < 2)
+		return ;
 	temp = stack->array[stack->size - 1];
 	ft_memmove(stack->array + 1, stack->array, stack->size * sizeof(int32_t));
 	stack->array[0] = temp;

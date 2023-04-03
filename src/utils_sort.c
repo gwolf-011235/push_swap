89cd8ccd87c11b5939ef332a17dde49d6601c75a
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-04-03 22:24:34 +0200

    feat: added new ft rot bottom and find neighbors

--- src/utils_sort.c
+++ src/utils_sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils_sort.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gwolf < gwolf@student.42vienna.com >       +#+  +:+       +#+        */
+/*   By: gwolf <gwolf@student.42vienna.com >        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 08:50:36 by gwolf             #+#    #+#             */
-/*   Updated: 2023/03/31 14:06:43 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/04/03 22:15:18 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,4 +54,64 @@ bool	ft_rotate_to_top(uint8_t name, t_stacks *stacks, uint32_t num)
 	else
 		ft_bust_some_moves(move[1], stacks, stack->size - position);
 	return (true);
+}
+
+bool	ft_rotate_to_bottom(uint8_t name, t_stacks *stacks, uint32_t num)
+{
+	uint32_t	position;
+	t_stack		*stack;
+	uint8_t		move[2];
+
+	if (name == A)
+	{
+		stack = &stacks->a;
+		move[0] = RA;
+		move[1] = RRA;
+	}
+	else
+	{
+		stack = &stacks->b;
+		move[0] = RB;
+		move[1] = RRB;
+	}
+	position = ft_search_from_top(stack, num);
+	if (position == 0)
+		return (false);
+	if (position < (stack->size / 2))
+		ft_bust_some_moves(move[0], stacks, position + 1);
+	else
+		ft_bust_some_moves(move[1], stacks, stack->size - position - 1);
+	return (true);
+}
+
+uint32_t	ft_find_higher_neighbor(uint32_t target, t_stack *stack)
+{
+	uint32_t	neighbor;
+	uint32_t	i;
+
+	neighbor = target;
+	i = 0;
+	while (i < stack->size)
+	{
+		if (stack->array[i] > neighbor)
+			neighbor = stack->array[i];
+		i++;
+	}
+	return (neighbor);
+}
+
+uint32_t	ft_find_lower_neighbor(uint32_t target, t_stack *stack)
+{
+	uint32_t	neighbor;
+	uint32_t	i;
+
+	neighbor = target;
+	i = 0;
+	while (i < stack->size)
+	{
+		if (stack->array[i] < neighbor)
+			neighbor = stack->array[i];
+		i++;
+	}
+	return (neighbor);
 }

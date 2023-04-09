46bd2edee0a2ccf9e3e68282ba79c8a3f7e1d03a
Gabriel Wolf <gabriel.cj.wolf@gmail.com>
2023-04-09 20:46:30 +0200

    fix: added driver ft for merge moves

--- src/merge_moves.c
+++ src/merge_moves.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 08:19:12 by gwolf             #+#    #+#             */
-/*   Updated: 2023/04/09 08:21:48 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/04/09 19:28:40 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,39 @@
 
 void	ft_merge_dupl_moves(t_queue *moves)
 {
+	ft_merge_yin_and_yang(moves, RA, RB, RR);
+	ft_merge_yin_and_yang(moves, RB, RA, RR);
+	ft_merge_yin_and_yang(moves, RRA, RRB, RRR);
+	ft_merge_yin_and_yang(moves, RRB, RRA, RRR);
+}
+
+void	ft_merge_yin_and_yang(t_queue *moves,
+			uint8_t yin_mv, uint8_t yang_mv, uint8_t whole)
+{
 	t_node	*yin;
 	t_node	*yang;
 	t_node	*remove;
 
-	yin = ft_find_dupl_yin(moves->head, RA);
+	yin = ft_find_yin(moves->head, yin_mv);
 	while (yin != NULL)
 	{
-		yang = ft_find_dupl_yang(yin, RB);
+		yang = ft_find_yang(yin, yin_mv, yang_mv, whole);
 		if (yang != NULL)
 		{
 			remove = yang->next;
 			yang->next = remove->next;
 			free(remove);
-			yin->value = RR;
+			yin->value = whole;
 		}
-		yin = ft_find_dupl_yin(yin->next, RA);
+		yin = ft_find_yin(yin->next, yin_mv);
 	}
 }
 
-t_node	*ft_find_dupl_yin(t_node *temp, uint8_t move)
+t_node	*ft_find_yin(t_node *temp, uint8_t yin_mv)
 {
 	if (temp == NULL)
 		return (NULL);
-	while (temp->value != move || temp->next != NULL)
+	while (temp->value != yin_mv && temp->next != NULL)
 		temp = temp->next;
 	if (temp->next == NULL)
 		return (NULL);
@@ -45,7 +54,8 @@ t_node	*ft_find_dupl_yin(t_node *temp, uint8_t move)
 		return (temp);
 }
 
-t_node	*ft_find_dupl_yang(t_node *temp, uint8_t move)
+t_node	*ft_find_yang(t_node *temp, uint8_t yin_mv, uint8_t yang_mv,
+			uint8_t whole)
 {
 	t_node	*remove;
 
@@ -54,9 +64,10 @@ t_node	*ft_find_dupl_yang(t_node *temp, uint8_t move)
 	while (temp->next != NULL)
 	{
 		remove = temp->next;
-		if (remove->value == move)
+		if (remove->value == yang_mv)
 			break ;
-		else if (remove->value == PA || remove->value == PB)
+		else if ((remove->value != yin_mv && remove->value != yang_mv
+				&& remove->value != whole) || remove->next == NULL)
 		{
 			remove = NULL;
 			break ;

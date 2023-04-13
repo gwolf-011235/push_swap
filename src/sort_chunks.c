1397fdad0d71e6aa18c2dae3f5241fd32ed239a6
Gabriel <gwolf@student.42vienna.com>
2023-04-13 07:00:21 +0200

    fix: in prep_chunks check for div=1; cleanup

--- src/sort_chunks.c
+++ src/sort_chunks.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 15:30:21 by gwolf             #+#    #+#             */
-/*   Updated: 2023/04/10 23:24:53 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/04/12 15:42:50 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,74 +31,18 @@ void	ft_prep_chunks(t_data *data)
 		data->chunks[i].pushed = 0;
 		i++;
 	}
-	data->chunks[i].size = size + (data->nums % data->div);
-	data->chunks[i].min = data->chunks[i - 1].max + 1;
-	data->chunks[i].max = data->nums;
-	data->chunks[i].pushed = 0;
+	if (data->div > 1)
+	{
+		data->chunks[i].size = size + (data->nums % data->div);
+		data->chunks[i].min = data->chunks[i - 1].max + 1;
+		data->chunks[i].max = data->nums;
+		data->chunks[i].pushed = 0;
+	}
 }
 
 bool	ft_is_elem_of_chunk(uint32_t num, t_chunk *chunk)
 {
 	return (num >= chunk->min && num <= chunk->max);
-}
-
-void	ft_chunk_update_bounds(t_data *data, t_chunk *chunk)
-{
-	if (chunk->pushed == 0)
-	{
-		chunk->bounds[0] = data->a.array[0];
-		chunk->bounds[1] = data->a.array[0];
-	}
-	else if (ft_is_elem_of_chunk(data->b.array[0], chunk))
-	{
-		if (!ft_is_elem_of_chunk(data->b.array[data->b.size - 1], chunk))
-			chunk->bounds[1] = data->a.array[0];
-	}
-	else if (ft_is_elem_of_chunk(data->b.array[data->b.size - 1], chunk))
-		chunk->bounds[0] = data->a.array[0];
-	else if (chunk->pushed == 1)
-		chunk->bounds[1] = data->a.array[0];
-}
-
-void	ft_chunk_prep_push(t_data *data, t_chunk *chunk, t_chunk *two)
-{
-	if (chunk->pushed == 0 && two->pushed == 0)
-		ft_bust_a_move(PB, data);
-	else if (chunk->pushed == 0)
-	{
-		ft_rotate_to_top(B, data, two->bounds[1]);
-		ft_bust_a_move(PB, data);
-	}
-	else if (ft_is_elem_of_chunk(data->b.array[0], chunk))
-		ft_bust_a_move(PB, data);
-	else if (ft_is_elem_of_chunk(data->b.array[data->b.size - 1], chunk))
-		ft_bust_a_move(PB, data);
-	else
-	{
-		ft_rotate_to_top(B, data, chunk->bounds[0]);
-		ft_bust_a_move(PB, data);
-	}
-	chunk->pushed++;
-}
-
-void	ft_presort_chunks(t_data *data, t_chunk *one, t_chunk *two)
-{
-	while ((one->pushed < one->size))
-	{
-		if (ft_is_elem_of_chunk(data->a.array[0], one))
-		{
-			ft_chunk_update_bounds(data, one);
-			ft_chunk_prep_push(data, one, two);
-		}
-		else if (ft_is_elem_of_chunk(data->a.array[0], two))
-		{
-			ft_chunk_update_bounds(data, two);
-			ft_chunk_prep_push(data, two, one);
-		}
-		else
-			ft_bust_a_move(RA, data);
-	}
-	ft_rotate_to_top(B, data, one->bounds[1]);
 }
 
 void	ft_quick_chunk_sort(t_data *data, t_chunk *one, t_chunk *two)

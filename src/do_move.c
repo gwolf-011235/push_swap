1a638345cdeab060344c70c5d887dedff9cf7f28
Gabriel <gwolf@student.42vienna.com>
2023-04-13 07:25:03 +0200

    fix: mv ft return move string into do_move

--- src/do_move.c
+++ src/do_move.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 09:12:19 by gwolf             #+#    #+#             */
-/*   Updated: 2023/04/12 17:36:44 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/04/13 07:24:18 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,4 +42,32 @@ void	ft_bust_a_move(uint8_t move, t_data *data)
 		ft_checker_talks(data, false);
 	if (!ft_enqueue(&data->moves, move))
 		ft_cleanup_and_leave(data, true);
+}
+
+char	*ft_return_move_string(uint8_t move)
+{
+	if (move == SA)
+		return ("sa\n");
+	else if (move == SB)
+		return ("sb\n");
+	else if (move == SS)
+		return ("ss\n");
+	else if (move == PA)
+		return ("pa\n");
+	else if (move == PB)
+		return ("pb\n");
+	else if (move == RA)
+		return ("ra\n");
+	else if (move == RB)
+		return ("rb\n");
+	else if (move == RR)
+		return ("rr\n");
+	else if (move == RRA)
+		return ("rra\n");
+	else if (move == RRB)
+		return ("rrb\n");
+	else if (move == RRR)
+		return ("rrr\n");
+	else
+		return (NULL);
 }
--- src/logic.c
+++ src/logic.c
@@ -6,7 +6,7 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 00:04:45 by gwolf             #+#    #+#             */
-/*   Updated: 2023/04/09 08:23:58 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/04/13 07:24:08 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,32 +24,4 @@ bool	ft_is_sorted(t_stack *stack)
 		i++;
 	}
 	return (true);
-}
-
-char	*ft_return_move_string(uint8_t move)
-{
-	if (move == SA)
-		return ("sa\n");
-	else if (move == SB)
-		return ("sb\n");
-	else if (move == SS)
-		return ("ss\n");
-	else if (move == PA)
-		return ("pa\n");
-	else if (move == PB)
-		return ("pb\n");
-	else if (move == RA)
-		return ("ra\n");
-	else if (move == RB)
-		return ("rb\n");
-	else if (move == RR)
-		return ("rr\n");
-	else if (move == RRA)
-		return ("rra\n");
-	else if (move == RRB)
-		return ("rrb\n");
-	else if (move == RRR)
-		return ("rrr\n");
-	else
-		return (NULL);
 }

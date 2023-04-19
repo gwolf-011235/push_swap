239aba37bf23a28bc5f1576eafd8208ef3ddb0d7
Gabriel <gwolf@student.42vienna.com>
2023-04-19 10:54:44 +0200

    fix: deleted move macros -> enum

--- include/push_swap_macros.h
+++ include/push_swap_macros.h
@@ -6,29 +6,19 @@
 /*   By: gwolf <gwolf@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 07:16:59 by gwolf             #+#    #+#             */
-/*   Updated: 2023/04/13 07:18:28 by gwolf            ###   ########.fr       */
+/*   Updated: 2023/04/19 10:54:28 by gwolf            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_MACROS_H
 # define PUSH_SWAP_MACROS_H
 
+//array masks
 # define A 0
 # define B 1
 # define SUM 2
 
-# define SA 0
-# define SB 1
-# define SS 2
-# define PA 3
-# define PB 4
-# define RA 5
-# define RB 6
-# define RR 7
-# define RRA 8
-# define RRB 9
-# define RRR 10
-
+//colors
 # define RESET "\033[0m"
 # define GREEN "\033[32m"
 # define YELLOW "\033[33m"
